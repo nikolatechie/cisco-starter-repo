15c6f6fc8652f9d9b91db669ec64322805af6b12
nikolatechie <nikola-grujic@hotmail.com>
2023-02-24 17:46:37 +0100

    Created a component that displays latency

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
 import IpAddress from './IpAddress';
+import PylonConnector from './PylonConnector';
 
 class App extends Component {
   render() {
@@ -11,7 +12,7 @@ class App extends Component {
         <Banner bannerText="Sextant"></Banner>
         <Exhibit name="First exhibit" children={<IpAddress version='v4' />}></Exhibit>
         <Exhibit name="Second exhibit" children={<IpAddress version='v6' />}></Exhibit>
-        <Exhibit name="Third exhibit"></Exhibit>
+        <Exhibit name="Third exhibit" children={<PylonConnector />}></Exhibit>
       </div>
     );
   }
---
+++ sextant/src/PylonConnector.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+const client = new W3CWebSocket('ws://localhost:55455');
+
+export default class PylonConnector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latency: null,
+    }
+  }
+
+  componentDidMount() {
+    client.onmessage = (message) => {
+      this.setState({
+        latency: new Date().getTime() - message.data,
+      })
+    }
+  }
+
+  render() {
+    return <span>{this.state.latency}</span>
+  }
+}
